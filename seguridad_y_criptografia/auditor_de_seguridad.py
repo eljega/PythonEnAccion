65c8ef1cd1a53322fd650c32991519cebc4d6163
eljega <playgames.j.g17@gmail.com>
2024-02-06 22:13:46 -0500

    cambios en el script de auditor de seguridad

--- seguridad_y_criptografia/auditor_de_seguridad.py
+++ seguridad_y_criptografia/auditor_de_seguridad.py
@@ -6,16 +6,16 @@ usando Bandit para buscar vulnerabilidades o malas practicas de seguridad
 Instrucciones de uso:
 1. debemos instalar bandit en nuestro venv.
 2. recuerda modificar la variable 'directorio_objetivo' ingresando la carpeta que contiene tus scripts de python.
-3. Ejecuta este script y luego de unos segundos "o minutos" los resultados del analisis de seguridad se mostraran en la consola.
+3. ejecuta este script y luego de unos segundos "o minutos" los resultados del analisis de seguridad se mostraran en la consola.
 
-Dependencias:
+dependencias:
 - bandit: `pip install bandit`
 """
 
 import subprocess
 import sys
-# ingresa la ruta de la carpeta de tu script o proyecto
-directorio_objetivo = 'C:/Users/playg/Escritorio/trabajo/python/repo_github/visualizacion_de_datos'
+# ingresamos la ruta de la carpeta de tu script o proyecto
+directorio_objetivo = 'aqui escribes la ruta relativa de tu carpeta o proyecto'
 
 def analizar_con_bandit(directorio):
     comando = ['bandit', '-r', directorio]
@@ -27,13 +27,13 @@ def analizar_con_bandit(directorio):
         print("Bandit encontro uno o mas problemas de seguridad o malas practicas:")
         print(e.stdout)
         if e.stderr:
-            print("Error/Informacion adicional de Bandit:")
+            print("Erroren la informacion adicional de Bandit:")
             print(e.stderr)
 
 if __name__ == "__main__":
     # aqui verificamos si el directorio es valido
     if not directorio_objetivo:
-        print("Por favor, configura el directorio_objetivo antes de ejecutar el script.")
+        print(" asegurate de configurar bien la ruta de tu carpeta")
         sys.exit(1)
     # y ejecutamos la verificacion
     analizar_con_bandit(directorio_objetivo)
